1366ecb07f313db900c299c17f79d19bf0dcd641
Nazri <abienazri23@gmail.com>
2021-01-11 14:24:20 +0700

    Optimazing script

--- client.lua
+++ client.lua
@@ -23,16 +23,54 @@ end)
 
 Citizen.CreateThread(function()
     while true do
-
-		Citizen.Wait(5)
-		local ped = PlayerPedId()
-		    ESX.TriggerServerCallback('nz_autorevive:getConnectedEMS', function(amount)
-				if amount < Config.ServiceCount and GetEntityHealth(ped) <= 1 then
-					Citizen.Wait(10)
-					--revivePed(ped)
-					TriggerEvent('esx_ambulancejob:revive', ped)
-					--RespawnPed(PlayerPedId(), formattedCoords, ESX.SetPlayerData('lastPosition', formattedCoords))
-				end
-		    end)
+		Citizen.Wait(500)
+		ESX.TriggerServerCallback('nz_autorevive:getConnectedEMS', function(amount)
+			local ped = PlayerPedId()
+			if amount < Config.ServiceCount and IsEntityDead(ped) then
+				Citizen.Wait(10)
+				TriggerEvent('nz_autorevive:revive', ped)
+			end
+		end)
     end
 end)
+
+AddEventHandler('nz_autorevive:revive', function()
+	local playerPed = PlayerPedId()
+	local coords = GetEntityCoords(playerPed)
+
+	--TriggerServerEvent('esx_ambulancejob:setDeathStatus', false)
+
+	Citizen.CreateThread(function()
+		DoScreenFadeOut(800)
+
+		while not IsScreenFadedOut() do
+			Citizen.Wait(50)
+		end
+
+		local formattedCoords = {
+			x = ESX.Math.Round(coords.x, 1),
+			y = ESX.Math.Round(coords.y, 1),
+			z = ESX.Math.Round(coords.z, 1)
+		}
+
+		ESX.SetPlayerData('lastPosition', formattedCoords)
+
+		TriggerServerEvent('esx:updateLastPosition', formattedCoords)
+
+		RespawnPed(playerPed, formattedCoords, 0.0)
+
+		StopScreenEffect('DeathFailOut')
+		DoScreenFadeIn(800)
+	end)
+end)
+
+function RespawnPed(ped, coords, heading)
+	SetEntityCoordsNoOffset(ped, coords.x, coords.y, coords.z, false, false, false, true)
+	NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, heading, true, false)
+	SetPlayerInvincible(ped, false)
+	ClearPedBloodDamage(ped)
+
+	TriggerServerEvent('esx:onPlayerSpawn')
+	TriggerEvent('esx:onPlayerSpawn')
+	TriggerEvent('playerSpawned') -- compatibility with old scripts, will be removed soon
+end
